5386cd26765d0d73bfe23f1aee440c2d6a23b6c4
CaioHappmobi <caiohen.ms@gmail.com>
2021-11-09 21:39:49 -0300

    feat: added faunadb

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,10 +1,12 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession } from 'next-auth/client'
 import styles from './styles.module.scss';
 
 export function SingInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const [session] = useSession();
+
+    return session ? (
         <button
             type="button"
             className={styles.singInButton}
@@ -17,6 +19,7 @@ export function SingInButton() {
         <button
             type="button"
             className={styles.singInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sing in with GitHub
